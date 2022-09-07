7a6cfcaf561be6b9f7930b1e825462ba8631988c
Jacob Andersen <jacob@algorithmjunkie.com>
2022-09-07 12:27:33 +0800

    Can't use Vite features in astro config, also, don't need Google Analytics. Move Google Fonts into MetaTags. Reenable import path aliasing.

--- src/config.mjs
+++ src/config.mjs
@@ -1,16 +1,9 @@
-const PROD = import.meta.env.PROD;
-
 export default {
-  name: "MCBanners",
-  
   origin: "https://mcbanners.com",
   basePathName: "/",
-
   upstreamOrigin: "https://api.mcbanners.com",
-  
+
+  name: "MCBanners",
   title: "MCBanners - Generate beautiful banners from backend statistics",
-  description: "MCBanners is a free - as in free beer, and as in libre - banner creation software stack that allows you to create beautiful banners from backend statistics. We support a variety of services with more coming all the time.",
-  
-  googleAnalyticsId: PROD ? import.meta.env.GOOGLE_ANALYTICS_ID : false,
-  googleSiteVerificationId: PROD ? import.meta.env.GOOGLE_SITE_VERIFICATION_ID : false
+  description: "MCBanners is a free - as in free beer, and as in libre - banner creation software stack that allows you to create beautiful banners from backend statistics. We support a variety of services with more coming all the time."
 }
