e5a25a0bd6bcf8041eb17e7b13e131bdc05021c9
darbyjack <admin@glaremasters.me>
2023-04-09 18:07:27 -0500

    fix: reduce bundle size for fontawesome

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,10 +1,10 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faHand, faPaintBrush, faFont, faPlus, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-library.add(fas)
+library.add(faHand, faPaintBrush, faFont, faPlus, faCheck)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
