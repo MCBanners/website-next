18676517387072b77e9970e065e2ec2aed613110
Jacob Andersen <jacob@algorithmjunkie.com>
2023-04-11 21:49:21 -0700

    invert logo color on dark mode

--- components/core/structure/Header.vue
+++ components/core/structure/Header.vue
@@ -15,7 +15,7 @@
     <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
             <NuxtLink href="/" class="flex items-center">
-                <img src="/img/mcbanners-logo.svg" class="h-16 mr-3 sm:h-9" alt="MCBanners Logo">
+                <img src="/img/mcbanners-logo.svg" class="h-16 mr-3 sm:h-9 dark:invert" alt="MCBanners Logo">
             </NuxtLink>
 
             <div class="flex md:order-2">
